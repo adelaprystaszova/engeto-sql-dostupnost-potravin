53079526088fd00dc9d8cd02960b32f1ee15ebb3
Adéla Prystaszová <adelaprystaszova@gmail.com>
2024-08-12 16:57:16 +0200

    Spojení předešlých dotazů ke tvorbě primární tabulky

--- projekt1_kod.sql
+++ projekt1_kod.sql
@@ -1,66 +1,72 @@
--- TVORBA PRIMIMÁRNÍ TABULKY
-
--- data pro otázku:
--- 1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
-CREATE OR REPLACE VIEW v_adela_prystaszova_1 AS 
-SELECT 
-	industry_branch_code,
-	payroll_year,
-	avg(value) average_wage
-FROM czechia_payroll
-WHERE value_type_code = 5958 
-	AND calculation_code = 100
-	AND industry_branch_code IS NOT NULL
-GROUP BY 
-	industry_branch_code, 
-	payroll_year
+-- Primární tabulka
+CREATE OR REPLACE TABLE t_adela_prystaszova_project_SQL_primary_final
+WITH wages_in_industries AS (
+	WITH wages AS (
+		SELECT 
+			industry_branch_code,
+			payroll_year,
+			avg(value) average_wage
+		FROM czechia_payroll
+		WHERE value_type_code = 5958  AND calculation_code = 100
+			AND industry_branch_code IS NOT NULL
+		GROUP BY 
+			industry_branch_code, 
+			payroll_year
+		ORDER BY 
+			industry_branch_code, 
+			payroll_year
+	)
+	SELECT 
+		cpib.name industry_branch_name,
+		w.payroll_year,
+		w.average_wage
+	FROM wages w
+	LEFT JOIN czechia_payroll_industry_branch cpib
+		ON w.industry_branch_code = cpib.code
+),
+food_prices AS (
+	WITH prices AS (
+		SELECT 
+			category_code,
+			avg(value) average_price,
+			year(date_from) price_year
+		FROM czechia_price
+		GROUP BY 
+			category_code, 
+			price_year
+	)
+	SELECT 
+		cpc.name,
+		p.average_price,
+		cpc.price_value,
+		cpc.price_unit,
+		p.price_year
+	FROM prices p
+	LEFT JOIN czechia_price_category cpc
+		ON p.category_code = cpc.code
+),
+economy AS (
+	SELECT
+		YEAR,
+		GDP
+	FROM economies
+	WHERE country = 'Czech Republic'
+)
+SELECT
+	wii.industry_branch_name,
+	wii.payroll_year,
+	wii.average_wage,
+	fp.name,
+	fp.average_price,
+	fp.price_value,
+	fp.price_unit,
+	ec.gdp
+FROM wages_in_industries wii
+INNER JOIN food_prices fp
+	ON wii.payroll_year = fp.price_year
+LEFT JOIN economy ec
+	ON wii.payroll_year = ec.year
 ORDER BY 
-	industry_branch_code, 
-	payroll_year
+	wii.industry_branch_name,
+	wii.payroll_year
 ;
-
-SELECT
-	cpib.name industry_branch_name,
-	v1.payroll_year,
-	v1.average_wage
-FROM v_adela_prystaszova_1 v1
-LEFT JOIN czechia_payroll_industry_branch cpib
-	ON v1.industry_branch_code = cpib.code
-;
-
--- data navíc pro otázky:
--- 2) Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
--- 3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
-
-CREATE OR REPLACE VIEW v_adela_prystaszova_2 as
-SELECT 
-	category_code,
-	avg(value) average_price,
-	year(date_from) price_year
-FROM czechia_price cp
-GROUP BY 
-	category_code, 
-	price_year
-;
-
-SELECT 
-	cpc.name,
-	v2.average_price,
-	cpc.price_value,
-	cpc.price_unit,
-	v2.price_year
-FROM v_adela_prystaszova_2 v2
-JOIN czechia_price_category cpc
-	ON v2.category_code = cpc.code
-;
-
-/* data navíc pro otázky:
- * 4) Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
- * 5) Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce,
- * projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
- */
-SELECT
-	YEAR,
-	GDP
-FROM economies
-WHERE country = 'Czech republic';

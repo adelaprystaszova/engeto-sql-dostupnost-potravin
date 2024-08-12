3cfbc5909f521f2f6d4a863eb868b2615fe951e2
Adéla Prystaszová <adelaprystaszova@gmail.com>
2024-08-12 16:45:16 +0200

    Úprava dotazů pro získání dat k otázkám

--- projekt1_kod.sql
+++ projekt1_kod.sql
@@ -2,12 +2,14 @@
 
 -- data pro otázku:
 -- 1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+CREATE OR REPLACE VIEW v_adela_prystaszova_1 AS 
 SELECT 
 	industry_branch_code,
 	payroll_year,
-	value
+	avg(value) average_wage
 FROM czechia_payroll
 WHERE value_type_code = 5958 
+	AND calculation_code = 100
 	AND industry_branch_code IS NOT NULL
 GROUP BY 
 	industry_branch_code, 
@@ -17,11 +19,20 @@ ORDER BY
 	payroll_year
 ;
 
+SELECT
+	cpib.name industry_branch_name,
+	v1.payroll_year,
+	v1.average_wage
+FROM v_adela_prystaszova_1 v1
+LEFT JOIN czechia_payroll_industry_branch cpib
+	ON v1.industry_branch_code = cpib.code
+;
+
 -- data navíc pro otázky:
 -- 2) Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 -- 3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
-CREATE OR REPLACE VIEW v_adela_prystaszova_1 as
+CREATE OR REPLACE VIEW v_adela_prystaszova_2 as
 SELECT 
 	category_code,
 	avg(value) average_price,
@@ -34,13 +45,13 @@ GROUP BY
 
 SELECT 
 	cpc.name,
-	v1.average_price,
+	v2.average_price,
 	cpc.price_value,
 	cpc.price_unit,
-	v1.price_year
-FROM v_adela_prystaszova_1 v1
+	v2.price_year
+FROM v_adela_prystaszova_2 v2
 JOIN czechia_price_category cpc
-	ON v1.category_code = cpc.code
+	ON v2.category_code = cpc.code
 ;
 
 /* data navíc pro otázky:
